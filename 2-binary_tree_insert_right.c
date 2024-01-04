33caf3c6f6a9441e3ea57e1771c97c28242f3a82
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-01-04 08:07:30 -0800

    Added Task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -28,7 +28,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (old)
 	{
 		old->parent = new;
-		new->left = old;
+		new->right = old;
 	}
 	parent->right = new;
 	return (new);
