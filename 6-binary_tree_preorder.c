16b18c4be36844027873648163e53eb7a179987a
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-01-04 11:53:28 -0800

    Added Task 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -9,9 +9,12 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree)
-		return;
-	func(tree->n);
-	binary_tree_preorder(tree->left, func);
-	binary_tree_preorder(tree->right, func);
+	if (func)
+	{
+		if (!tree)
+			return;
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
 }
