93eeb48e91ce3085f6724827b5cc81d019ea3d1f
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-01-03 16:17:50 -0800

    Added Task 0 Write a function that creates a tree node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates new tree node.
+ * @parent: A pointer to the parent of the mode to create.
+ * @value: The value to put in the new node.
+ *
+ * Return: A pointer to the new node on success, otherwise NULL.
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = NULL;
+
+	new = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+
+	return (new);
+}
