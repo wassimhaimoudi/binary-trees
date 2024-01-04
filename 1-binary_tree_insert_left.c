d74b66b10f3039e09a4265f5eeed8f0385ba9692
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-01-04 06:27:54 -0800

    Fixing task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,7 +21,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left)
 	{
 		new->left = parent->left;
-		parent->left->parent = new;
+		parent = new;
 	}
 	parent->left = new;
 	return (new);
